2037813c11dc6c8500c611f3c8025a33cb8b8489
kajal-2011 <kajalrekha888@gmail.com>
2023-06-20 00:30:15 +0600

    footer added

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,8 +1,8 @@
 const Footer = () => {
   return (
-    <footer className="py-8 flex flex-col gap-3 items-center bg-rose-200 opacity-75">
-      <h2 className="italic text-2xl font-semibold">
-        taste<span className="text-rose-500">scape</span>
+    <footer className="py-8 flex flex-col gap-3 items-center bg-teal-900">
+      <h2 className="text-2xl font-semibold">
+        taste<span className="">scape</span>
       </h2>
       <p>&copy; {new Date().getFullYear()} tastescape.All rights reserved</p>
     </footer>
