43b3cc3466508ab1fbd19f0d388e9965ccbeb23e
kajal-2011 <kajalrekha888@gmail.com>
2023-06-20 16:48:30 +0600

    checkout form done

--- pages/checkout/[mealId].js
+++ pages/checkout/[mealId].js
@@ -28,26 +28,34 @@ const Checkout = ({ meal }) => {
 
   return (
     <div className="wrapper py-10 min-h-screen">
-      <SectionHeader
-        span={"Checkout"}
-        h2={"Personalize Your Order by Sharing Your Details!"}
-        p={"Unlock Exclusive Benefits: Complete the Checkout Form Now!"}
-      />
-      <div className="checkout-form  grid grid-cols-1 md:grid-cols-2 gap-5 items-center">
-        <div className="checkout-left hidden md:block w-full h-screen overflow-hidden mt-14 ">
-          <Image
-            src="https://res.cloudinary.com/dh0ado2ir/image/upload/v1687241238/photo-1589010588553-46e8e7c21788_qlvzuz.jpg"
-            alt="logo"
-            width={500}
-            height={500}
-            className="w-full h-full object-cover rounded-lg"
-            priority
-          />
+      <h3 className="text-center text-2xl">Checkout</h3>
+      <p className="text-center">{`Indulge in Tastescape's Delectable Meal Haven!`}</p>
+      <div className="checkout-form  grid grid-cols-1 md:grid-cols-2 gap-8 items-center mt-10">
+        <div
+          style={{
+            backgroundImage: `url("https://res.cloudinary.com/dh0ado2ir/image/upload/v1687257702/photo-1589010588553-46e8e7c21788-removebg-preview_1_egb70d.png")`,
+          }}
+          className="w-full h-[43rem] justify-center items-center bg-cover bg-no-repeat bg-center relative mt-10 object-cover overflow-hidden"
+        >
+          <p className="text-2xl mt-5 text-center">Tastescape</p>
+          <p className="text-lg absolute bottom-5 p-2">
+            Tastescape is an immersive online food shop that takes you on a
+            delectable journey of flavors. With our carefully curated selection
+            of culinary delights, we bring the world of gastronomy to your
+            doorstep. From tantalizing appetizers to mouthwatering main courses
+            and decadent desserts, Tastescape offers a wide range of gourmet
+            options to satisfy every palate. Experience the ultimate food
+            adventure with Tastescape and indulge in a symphony of tastes that
+            will leave you craving for more.
+          </p>
         </div>
 
         <div className="checkout-right flex justify-center">
-          <form className="flex flex-col gap-5 mt-10 w-full lg:w-[35rem]">
-            <div className="form-control flex flex-col gap-2">
+          <form className="flex flex-col gap-5  w-full lg:w-[35rem]">
+            <div className="form-control flex flex-col gap-2 mt-10">
+              <p className="text-xl">
+                Unlock Exclusive Benefits: Checkout Now, Complete Form!
+              </p>
               <label htmlFor="name" className="cursor-pointer">
                 Name
               </label>
@@ -111,13 +119,13 @@ const Checkout = ({ meal }) => {
 
             <div className="form-control flex flex-col gap-2">
               <label htmlFor="address" className="cursor-pointer">
-                Course title
+                Meal title
               </label>
               <input
                 type="text"
-                id="courseTitle"
-                placeholder="Mastering Graphic Design"
-                value={formData.courseTitle}
+                id="mealTitle"
+                placeholder="vegitable curry"
+                value={formData.mealTitle}
                 readOnly
                 className="bg-transparent outline-none border py-3 px-4 rounded-lg focus:border-gray-700"
               />
